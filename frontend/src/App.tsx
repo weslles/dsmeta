960b137ef2422cc1e9d46267fcad4eb20a628cc5
weslles <joseweslles.b2@gmail.com>
2022-09-21 20:26:27 -0300

    First Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,14 @@
+import NotificationButton from "./components/Notification Buutton"
+
 function App() {
   return (
+    <>
       <h1>Ola mundo!</h1>
-    )
+      <NotificationButton></NotificationButton>
+      <NotificationButton></NotificationButton>
+    </>
+  )
+
 }
 
 export default App
---
+++ frontend/src/components/Notification Buutton/index.tsx
@@ -0,0 +1,14 @@
+import icon from '../../assets/img/notificar.svg'
+import './styles.css'
+
+function NotificationButton() {
+    return (
+        <>
+            <div className="dsmeta-red-btn">
+                <img src={icon} alt="Notificar"></img>
+            </div>
+        </>
+    )
+}
+
+export default NotificationButton
