2a551a91a641ba2612c6f693643bec53655848ce
weslles <joseweslles.b2@gmail.com>
2022-09-22 00:03:56 -0300

    useState

--- frontend/src/components/Notification Buutton/index.tsx
+++ frontend/src/components/Notification Buutton/index.tsx
@@ -1,5 +1,5 @@
-import icon from '../../assets/img/notificar.svg'
-import './styles.css'
+import icon from '../../assets/img/notificar.svg';
+import './styles.css';
 
 function NotificationButton() {
     return (
@@ -11,4 +11,4 @@ function NotificationButton() {
     )
 }
 
-export default NotificationButton
+export default NotificationButton;
